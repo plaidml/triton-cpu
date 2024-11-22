b23fbf225f7c602a024f990566ae48479f7fcea2
Renato Golin <rengolin@systemcall.eu>
2024-11-22 18:43:36 +0000

    add time to bencharmk to help with predictions

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -8,12 +8,12 @@ for datatype in f32 bf16; do
   for num_threads in 1 $(nproc); do
     for benchmark in xsmm-scalar xsmm-block; do
       echo -e "\n\nRUN: $benchmark | threads $num_threads | type $datatype"
-      $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $benchmark $num_threads --datatype $datatype $external_pad
+      time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $benchmark $num_threads --datatype $datatype $external_pad
     done
     for benchmark in xsmm-pad-k xsmm-loop-collapse-pad-b; do
       for external_pad in "" "--external-pad"; do
         echo -e "\n\nRUN: $benchmark | threads $num_threads | type $datatype | $external_pad"
-        $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $benchmark $num_threads --datatype $datatype $external_pad
+        time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $benchmark $num_threads --datatype $datatype $external_pad
       done
     done
   done
